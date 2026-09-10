9f2b875f8e7c80a4ab009473ccac12448a197616
Karl McDowall <karl.mcdowall@gmail.com>
2026-09-10 12:28:13 -0600

    WIP

--- HW_2.py
+++ HW_2.py
@@ -5,6 +5,11 @@ from scipy.linalg import expm
 k = 398600
 r_0 = 6678
 
-Omega_0 = np.sqrt(k/(r_0^3))
+Omega_0_squared = k/(r_0^3)
+Omega_0 = np.sqrt(Omega_0_squared)
 
 print(f"Omega_0 = {Omega_0}")
+
+A = [[0, 1, 0, 0], [3 * Omega_0_squared, 0, 0, 2]]
+
+print(f"A = {A}")
