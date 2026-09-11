d98aaa6ac94fd6d2b862a4e69fe741b71ac9b72a
Karl McDowall <karl.mcdowall@gmail.com>
2026-09-11 10:32:49 -0600

    WIP

--- HW_2.py
+++ HW_2.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 from scipy.linalg import expm
 from scipy.integrate import solve_ivp
 
+
 def main():
     # Define constants
     k = 398600.0
@@ -47,10 +48,53 @@ def main():
         time += delta_t
         current_delta_x = np.matmul(STM, current_delta_x)
 
-    # plt.plot(timestamps, x_nom_plus_delta_x, label=r"$\Delta p$", color="red")
-    # plt.plot(timestamps, X_q, label=r"$\Delta q$", color='green')
-    # plt.plot(timestamps, X_r, label=r"$\Delta r$", color='blue')
-    # plt.show()
+    delta_theta_dot = [delta[3] for delta in delta_x]
+    # Make plots of pertubation-states vs time
+    fig, ax1 = plt.subplots()
+    ax1.set_xlabel("Time (s)")
+    r_color, r_dot_color, theta_color, theta_dot_color = "red", "blue", "orange", "green"
+
+    delta_r = [delta[0] for delta in delta_x]
+    ax1.set_ylabel("Distance ($km$)", color=r_color)
+    ax1.tick_params(axis='y', labelcolor=r_color)
+    ax1.spines[["top", "right"]].set_visible(False)
+    ax1.spines["left"].set_color(r_color)
+    line1, = ax1.plot(timestamps, delta_r, label=r"$\Delta r$", color=r_color, linewidth='0.5')
+
+    ax2 = ax1.twinx()
+    delta_r_dot = [delta[1] for delta in delta_x]
+    ax2.yaxis.tick_left()
+    ax2.yaxis.set_label_position("left")
+    ax2.spines["left"].set_position(("axes", -0.15))
+    ax2.spines["left"].set_color(r_dot_color)
+    ax2.spines[["top", "right"]].set_visible(False)
+    ax2.set_ylabel(r"Speed ($km s^{-1}$)", color=r_dot_color)
+    ax2.tick_params(axis='y', labelcolor=r_dot_color)
+    line2, = ax2.plot(timestamps, delta_r_dot, label=r"$\Delta \dot{r}$", color=r_dot_color, linewidth='0.5')
+
+    ax3 = ax1.twinx()
+    delta_theta = [delta[2] for delta in delta_x]
+    ax3.set_ylabel(r"Angle ($rad$)", color=theta_color)
+    ax3.spines["right"].set_color(theta_color)
+    ax3.spines[["top", "left"]].set_visible(False)
+    ax3.tick_params(axis='y', labelcolor=theta_color)
+    line3, = ax3.plot(timestamps, delta_theta, label=r"$\Delta\Theta$", color=theta_color, linewidth='0.5')
+
+    ax4 = ax1.twinx()
+    delta_theta_dot = [delta[3] for delta in delta_x]
+    ax4.set_ylabel(r"Angular Velocity ($rad/s$)", color=theta_dot_color)
+    ax4.spines["right"].set_position(("axes", 1.15))
+    ax4.spines["right"].set_color(theta_dot_color)
+    ax4.spines[["top", "left"]].set_visible(False)
+    ax4.tick_params(axis='y', labelcolor=theta_dot_color)
+    line4, = ax4.plot(timestamps, delta_theta_dot, label=r"$\Delta \dot{\Theta}$", color=theta_dot_color, linewidth='0.5')
+
+    fig.legend(handles=[line1, line2, line3, line4])
+    plt.title("Pertubation States vs. Time")
+    plt.tight_layout()
+    plt.show()
+
+    # Make plots of total system states vs time
 
     sol = solve_ivp(
         scipy_system,
@@ -59,8 +103,9 @@ def main():
         method="RK45",
         t_eval=np.linspace(0, 6000, 400),
     )
-    plt.plot(sol.t, sol.y[3], label="x(t)")
-    plt.show()
+    # plt.plot(sol.t, sol.y[3], label="x(t)")
+    # plt.show()
+
 
 def scipy_system(_t, S):
     k = 398600.0
