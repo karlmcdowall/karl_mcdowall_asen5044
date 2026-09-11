ada1ab5f57b37e5c00a95f4f161c1a0838a8877e
Karl McDowall <karl.mcdowall@gmail.com>
2026-09-11 12:59:42 -0600

    WIP

--- HW_2.py
+++ HW_2.py
@@ -51,13 +51,16 @@ def main():
     plot_pertubations(timestamps, delta_x)
     plot_expected_total_system_state(timestamps, x_nom_plus_delta_x)
 
+
     sol = solve_ivp(
         scipy_system,
-        [0, 6000],
+        [timestamps[0], timestamps[len(timestamps)-1]],
         [r_0 + r_pertubation, r_dot_pertubation, 0.0, omega_0 + theta_dot_pertubation],
         method="RK45",
-        t_eval=np.linspace(0, 6000, 400),
+        t_eval=timestamps,
     )
+    assert(len(timestamps) == len(sol.y[1]))
+    plot_rk4_total_system_state(timestamps, sol.y[0], sol.y[1], sol.y[2], sol.y[3])
     # plt.plot(sol.t, sol.y[3], label="x(t)")
     # plt.show()
 
@@ -155,6 +158,49 @@ def plot_expected_total_system_state(timestamps: list[int], x_nom_plus_delta_x:l
     plt.show()
 
 
+def plot_rk4_total_system_state(timestamps: list[int], r:np.ndarray, r_dot: np.ndarray, theta: np.ndarray, theta_dot: np.ndarray):
+    # Make plots of pertubation-states vs time
+    fig, ax1 = plt.subplots(figsize=(10, 5))
+    ax1.set_xlabel("Time (s)")
+    r_color, r_dot_color, theta_color, theta_dot_color = "red", "blue", "orange", "green"
+
+    ax1.set_ylabel("Distance ($km$)", color=r_color)
+    ax1.tick_params(axis='y', labelcolor=r_color)
+    ax1.spines[["top", "right"]].set_visible(False)
+    ax1.spines["left"].set_color(r_color)
+    line1, = ax1.plot(timestamps, r, label=r"$r$", color=r_color, linewidth='0.5')
+
+    ax2 = ax1.twinx()
+    ax2.yaxis.tick_left()
+    ax2.yaxis.set_label_position("left")
+    ax2.spines["left"].set_position(("axes", -0.15))
+    ax2.spines["left"].set_color(r_dot_color)
+    ax2.spines[["top", "right"]].set_visible(False)
+    ax2.set_ylabel(r"Speed ($km s^{-1}$)", color=r_dot_color)
+    ax2.tick_params(axis='y', labelcolor=r_dot_color)
+    line2, = ax2.plot(timestamps, r_dot, label=r"$\dot{r}$", color=r_dot_color, linewidth='0.5')
+
+    ax3 = ax1.twinx()
+    ax3.set_ylabel(r"Angle ($rad$)", color=theta_color)
+    ax3.spines["right"].set_color(theta_color)
+    ax3.spines[["top", "left"]].set_visible(False)
+    ax3.tick_params(axis='y', labelcolor=theta_color)
+    line3, = ax3.plot(timestamps, theta, label=r"$\Theta$", color=theta_color, linewidth='0.5')
+
+    ax4 = ax1.twinx()
+    ax4.set_ylabel(r"Angular Velocity ($rad/s$)", color=theta_dot_color)
+    ax4.spines["right"].set_position(("axes", 1.15))
+    ax4.spines["right"].set_color(theta_dot_color)
+    ax4.spines[["top", "left"]].set_visible(False)
+    ax4.tick_params(axis='y', labelcolor=theta_dot_color)
+    line4, = ax4.plot(timestamps, theta_dot, label=r"$\dot{\Theta}$", color=theta_dot_color, linewidth='0.5')
+
+    fig.legend(handles=[line1, line2, line3, line4])
+    plt.title("Expected Total System States vs. Time")
+    plt.tight_layout()
+    plt.show()
+
+
 def scipy_system(_t, S):
     k = 398600.0
 
@@ -164,7 +210,6 @@ def scipy_system(_t, S):
     d2r_dt2 = (r * theta_dot * theta_dot) - k / (r**2)
     d2theta_dt2 = -2 * theta_dot * r_dot / r
 
-    print(f"Theta = {theta}")
     return [dr_dt, d2r_dt2, dtheta_dt, d2theta_dt2]
 
 
